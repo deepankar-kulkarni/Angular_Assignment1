d874a8cb592f703672d7ade14f4037d6aa5aabc8
deepankar-kulkarni <deepankarkulkarni@gmail.com>
2018-06-21 01:19:31 +0530

    Assignment 1 : Q3  use inline html contents in app.componants.ts

---
+++ Assignment1/Q5/FirstApp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div style="text-align:center">
+                <h1>
+                  Welcome to Marvellous Infosystems!
+                </h1>
+                <h2>Educating for better tomorrow11 </h2>
+                </div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
